29bceba2f437a5e8859c947512c277442530ef4a
Sarah <sjbrooks305@gmail.com>
2020-03-09 17:45:33 -0700

    movie app with WIP sort function

--- movie-app.js
+++ movie-app.js
@@ -1,8 +1,10 @@
+let movieList = [];
+
 $('form').on("submit", (evt) => {
   evt.preventDefault();
   let $title = $('input:first-child').val();
   let $rating = $('input:nth-child(2)').val();
-  
+
   if ($title.length < 2) {
     alert("Please enter a valid movie title of at least 2 characters.");
     return;
@@ -13,25 +15,42 @@ $('form').on("submit", (evt) => {
     return;
   }
 
-  let $titleDiv = $('<div>').addClass('title').text($title);
-  let $movieDiv = $('<div>').addClass('movie').text($rating);
-  let $movieRatingDiv = $('<div>').addClass('movie-rating').append($titleDiv, $movieDiv);
-
-  $("#submitted-ratings").append($movieRatingDiv);
-
-  $('div.movie-rating').on("click", (evt) => {
-    $(evt.target).parent().remove();
-  });
+  let newMovie = new MovieRating($title, $rating);
+  movieList.push(newMovie);
+  $("#submitted-ratings").append(newMovie.createMovie());
 })
 
+$('.sort-alph').on("click", () => {
+  console.log("successful click;");
+  console.log(movieList);
+  movieList.sort((a,b) => {
+    (a.title > b.title ? 1 : -1);
+    console.log(movieList);
+  });
+  redrawDOM();
+});
 
-// $(function sort () {
-//   $('#sort-alph').on("click", () =>)
-// });
+function redrawDOM() {
+  $('#submitted-ratings').empty();
+  for (let movie of movieList) {
+    $('#submitted-ratings').append(movie.createMovie());
+  }
+};
 
+class MovieRating {
+  constructor(title, rating) {
+    this.title = title;
+    this.rating = rating;
+  }
 
+  createMovie() {
+    let $titleDiv = $('<div>').addClass('title').text(this.title);
+    let $movieDiv = $('<div>').addClass('movie').text(this.rating);
+    let $movieRatingDiv = $('<div>').addClass('movie-rating').append($titleDiv, $movieDiv);
 
-
-// Class for movies
-
-// on submit
+    $movieRatingDiv.on("click", (evt) => {
+      $(evt.target).parent().remove();
+    });
+    return $movieRatingDiv;
+  }
+}
